d3116e9a90e2c0e7fbbf9d5e9d23391ac2c600e7
amitmitra26 <poprincepersia7@gmail.com>
2017-08-13 17:24:11 +0530

    Recently Added Properties [issue:11]

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,11 +2,11 @@ class StaticPagesController < ApplicationController
   skip_before_action :require_login
 
   def home
-
+    @properties_recent = Property.where("created_at > (curdate() - 1)")
   end
   def search
     @search_properties_for = Property.where("(info = ? OR city = ? OR location = ? OR category = ? OR rooms = ?) OR (price BETWEEN ? AND ?)", params[:info], params[:city], params[:location], params[:type], params[:rooms], params[:min], params[:max])
-  
+
   end
   def help
   end
